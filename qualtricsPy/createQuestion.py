535fd82cdb3f8ae1e4a8700c98a51b7586f8c824
Joli Holmes <jh111@ad.rice.edu>
2019-05-16 10:20:51 -0500

    initial commit

---
+++ qualtricsPy/createQuestion.py
@@ -0,0 +1,29 @@
+import requests
+from qualtricsPy.utils import params, config, post
+
+
+class createQuestionParams(params, config):
+
+    def __init__(self):
+        """
+        Initializes the create question parameters
+        """
+        params.__init__(self)
+        config.__init__(self)
+        self.surveyId = self.addQuestion["id"]
+        self.endpoint = "https://{}.qualtrics.com/API/v3/survey-definitions/{}/questions"
+        self.endpoint = self.endpoint.format(self.dataCenter, self.surveyId)
+        self.data = self.addQuestion["qs"]["q1"]
+        self.headers = {"accept": "application/json",
+                        "content-type": "application/json"}
+        self.headers.update(self.authHeader)
+
+
+class createQuestion(createQuestionParams):
+
+    def __init__(self):
+        """
+        Creates a new question in the specified survey
+        """
+        createQuestionParams.__init__(self)
+        post(self.endpoint, self.data, self.headers)
