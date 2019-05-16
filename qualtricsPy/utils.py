8928a2a72c62d39ba8de1b95409445da56e862d1
Joli Holmes <jh111@ad.rice.edu>
2019-05-16 10:18:43 -0500

    added post function from classes

--- qualtricsPy/utils.py
+++ qualtricsPy/utils.py
@@ -3,6 +3,28 @@ import requests
 from utilsPy.config import read_yaml
 
 
+def post(endpoint, data, headers):
+    """
+    Posts to the API
+    :param endpoint: the endpoint of the post call
+    :type endpoint: string
+    :param data: the data to post
+    :type data: dct
+    :param headers: the headers to post
+    :type headers: dct
+    """
+
+    response = requests.post(url=endpoint,
+                             json=data,
+                             headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        pass
+    else:
+        print(response.text)
+        # self.response.raise_for_status()
+
+
 class config(object):
 
     def __init__(self):
@@ -11,6 +33,7 @@ class config(object):
         """
         self.config = read_yaml("config.yaml")
         self.copySurvey = self.config["copySurvey"]
+        self.updateSurvey = self.config["updateSurvey"]
         self.addQuestion = self.config["addQuestion"]
 
 
