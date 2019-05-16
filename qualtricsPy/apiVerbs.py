e3575c4064cbd2fd790e2283d3bd4c033c362f42
Joli Holmes <jh111@ad.rice.edu>
2019-05-16 12:04:41 -0500

    initial commit

---
+++ qualtricsPy/apiVerbs.py
@@ -0,0 +1,51 @@
+import requests
+
+
+def get(endpoint, headers):
+    """
+    GET to the API
+    """
+    response = requests.get(url=endpoint,
+                            headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print(response.text)
+        # self.response.raise_for_status()
+
+
+def post(endpoint, data, headers):
+    """
+    Posts to the API
+    :param endpoint: the endpoint of the post call
+    :type endpoint: string
+    :param data: the data to post
+    :type data: dct
+    :param headers: the headers to post
+    :type headers: dct
+    """
+    response = requests.post(url=endpoint,
+                             json=data,
+                             headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print(response.text)
+        # self.response.raise_for_status()
+
+
+def put(endpoint, data, headers):
+    """
+    PUT TO API
+    """
+    response = requests.post(url=endpoint,
+                             json=data,
+                             headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print(response.text)
+        # self.response.raise_for_status()
