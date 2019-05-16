b524a91d4d5da6e47f46cf61201f2c9dc7b55403
Joli Holmes <jh111@ad.rice.edu>
2019-05-16 09:42:22 -0500

    moved all the parameters into the params class

--- qualtricsPy/copySurvey.py
+++ qualtricsPy/copySurvey.py
@@ -8,9 +8,18 @@ class copySurveyParams(config):
         """
         Initializes the copy survey parameters
         """
+        params.__init__(self)
         config.__init__(self)
         self.surveyId = self.copySurvey["id"]
         self.surveyName = self.copySurvey["name"]
+        self.endpoint = "https://{0}.qualtrics.com/API/v3/surveys/"
+        self.endpoint = self.endpoint.format(self.dataCenter)
+        self.data = {"projectName": self.surveyName}
+        self.headers = {"content-type": "application/json",
+                        "x-copy-source": self.surveyId,
+                        "x-copy-destination": self.userId,
+                        "X-copy-destination-owner": self.userId}
+        self.headers.update(self.authHeader)
 
 
 class copySurvey(params, copySurveyParams):
@@ -19,19 +28,8 @@ class copySurvey(params, copySurveyParams):
         """
         Copys the survey and renames it
         """
-        params.__init__(self)
         copySurveyParams.__init__(self)
 
-        self.data = {"projectName": self.surveyName}
-        self.headers = {"content-type": "application/json",
-                        "x-copy-source": self.surveyId,
-                        "x-copy-destination": self.userId,
-                        "X-copy-destination-owner": self.userId}
-
-        self.headers.update(self.authHeader)
-
-        self.endpoint = "https://{0}.qualtrics.com/API/v3/surveys/"
-        self.endpoint = self.endpoint.format(self.dataCenter)
         self.response = requests.post(url=self.endpoint,
                                       json=self.data,
                                       headers=self.headers)
