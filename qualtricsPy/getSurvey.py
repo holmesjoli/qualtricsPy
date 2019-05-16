805d15c7a7991e30659935d615e611495d4bd573
Joli Holmes <jh111@ad.rice.edu>
2019-05-16 11:23:49 -0500

    initial commit

---
+++ qualtricsPy/getSurvey.py
@@ -0,0 +1,26 @@
+from qualtricsPy.utils import params, config, \
+    get, surveyEndpoint
+
+
+class getSurveyParams(params, config):
+
+    def __init__(self):
+        """
+        """
+        params.__init__(self)
+        config.__init__(self)
+        self.surveyId = self.getSurvey["id"]
+        self.endpoint = surveyEndpoint(self.dataCenter, self.surveyId)
+        self.headers = self.authHeader
+
+
+class getSurvey(getSurveyParams):
+
+    def __init__(self):
+        """
+        """
+        getSurveyParams.__init__(self)
+        get(self.endpoint, self.headers)
+
+
+
